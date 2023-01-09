279d80d2091afbab97240674759224df012ab0e3
Renārs Minkevics <49805886+vaktinsa@users.noreply.github.com>
2023-01-09 19:12:09 +0200

    Camera update
    Added basic camera, renamed some files to be more accurate. Added reactions to mouse input.
    TODO: reduce lag in camera, make it follow mouse 360 degrees.

--- timezone/Assets/Scenes/PlayerMovement.cs
+++ timezone/Assets/Scenes/PlayerMovement.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
+    public Camera frameCamera;
     public Rigidbody2D rb;
 
     private Vector2 moveDirection;
+
+    private Vector2 mousePosition;
 
 
     // Update is called once per frame
@@ -29,11 +32,17 @@ public class NewBehaviourScript : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized; 
+        mousePosition = frameCamera.ScreenToWorldPoint(Input.mousePosition);
     }
     
     void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+        //rotē spēlētāju pret peli
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
     }
 
 }
